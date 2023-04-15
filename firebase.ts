9f61a54471cbf39f8386c1c5a771419fb047d5d1
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-04-15 09:14:02 -0500

    Handle staging and prod environments

--- firebase.ts
+++ firebase.ts
@@ -11,7 +11,7 @@ import {
 } from "firebase/firestore";
 import { ChatMessage, MessageBase } from "judith/types";
 
-const firebaseConfig = {
+const prodConfig = {
   apiKey: "AIzaSyCmv2F5uK1aoyhkFMawZIo_94c1ovd09Uk",
   authDomain: "judith-beck.firebaseapp.com",
   projectId: "judith-beck",
@@ -19,6 +19,18 @@ const firebaseConfig = {
   messagingSenderId: "1080410332569",
   appId: "1:1080410332569:web:b02f5f3101000da97b0708",
 };
+
+const stagingConfig = {
+  apiKey: "AIzaSyDRFg4CuMHZy-qSkSCFhiAu3vGA-alLW_k",
+  authDomain: "judith-staging.firebaseapp.com",
+  projectId: "judith-staging",
+  storageBucket: "judith-staging.appspot.com",
+  messagingSenderId: "1015643850727",
+  appId: "1:1015643850727:web:46b75b71a42aeedc039f42",
+};
+
+const firebaseConfig =
+  process.env.NODE_ENV === "production" ? prodConfig : stagingConfig;
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
