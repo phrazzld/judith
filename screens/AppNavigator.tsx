cb97b4a698bd1d9e037f1568f6ed3ab312a9683d
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-04-16 09:15:20 -0500

    Mock voice chat screen

--- screens/AppNavigator.tsx
+++ screens/AppNavigator.tsx
@@ -1,8 +1,10 @@
 import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "judith/colors";
 import { createStackNavigator } from "@react-navigation/stack";
 import AccountScreen from "judith/screens/AccountScreen";
 import AuthScreen from "judith/screens/AuthScreen";
 import ChatScreen from "judith/screens/ChatScreen";
+import VoiceChatScreen from "judith/screens/VoiceChatScreen";
 import React from "react";
 import { TouchableOpacity } from "react-native";
 
@@ -20,7 +22,7 @@ const AppNavigator = () => {
         name="Chat"
         component={ChatScreen}
         options={({ navigation }) => ({
-          title: "Chat",
+          title: "Judith",
           headerShown: true,
           headerRight: () => (
             <TouchableOpacity onPress={() => navigation.navigate("Account")}>
@@ -32,7 +34,47 @@ const AppNavigator = () => {
               />
             </TouchableOpacity>
           ),
-          headerLeft: () => null,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("VoiceChat")}>
+              <Ionicons
+                name="mic-outline"
+                size={30}
+                color="black"
+                style={{ marginLeft: 15 }}
+              />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="VoiceChat"
+        component={VoiceChatScreen}
+        options={({ navigation }) => ({
+          title: "Judith",
+          headerStyle: {
+            backgroundColor: COLORS.tertiary,
+          },
+          headerShown: true,
+          headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Account")}>
+              <Ionicons
+                name="person-circle-outline"
+                size={30}
+                color="black"
+                style={{ marginRight: 15 }}
+              />
+            </TouchableOpacity>
+          ),
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+              <Ionicons
+                name="chatbubble-ellipses-outline"
+                size={30}
+                color="black"
+                style={{ marginLeft: 15 }}
+              />
+            </TouchableOpacity>
+          ),
         })}
       />
       <Stack.Screen
